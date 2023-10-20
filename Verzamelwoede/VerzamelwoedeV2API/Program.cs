e6cfe8f3f532d2e1ec29ca410e5bee61d6384b2a
Jaymian-Lee <jaymian-lee@ziggo.nl>
2023-10-20 14:58:03 +0200

    Fixed API

--- Verzamelwoede/VerzamelwoedeV2API/Program.cs
+++ Verzamelwoede/VerzamelwoedeV2API/Program.cs
@@ -1,13 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using VerzamelwoedeV2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
